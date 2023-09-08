6a10b8d9dba28ca89ef72972a1071c0e3b88802e
MelvinJessil <102487661+MelvinJessil@users.noreply.github.com>
2023-09-08 14:58:30 +1200

    Add files via upload

--- display.php
+++ display.php
@@ -28,61 +28,30 @@
 		<img src = "images/cart.png" width = "30px" height = "30px" >
 		
 	 </div>
-	
+		
 	<hr>
-		
-	<!-- FIRST SLIDESHOW -->
-	
-		<!-<div class = "slideshow-container">
-		<div class = "mySlides fade">
-			<div class = "numbertext"></div>
-			<a href ="sales.php">
-			<img src = "images/banner1.png" style = "width: 100%">
-			</a>
-		</div>
-		
-		<div class = "mySlides fade">
-			<div class = "numbertext"></div>
-			<a href ="display.php">
-			<img src = "images/banner22.png" style = "width: 100%">
-			</a>
-		</div>		
-
-		
-		<a class = "prev" onclick = "plusSlides(-1)">&#10094;</a> 
-		<a class = "next" onclick = "plusSlides(1)">&#10095;</a> 
-		
-	</div>
-	<br>
-	
-		<div style = "text-align:center">
-			<span class = "dot" onclick="currentSlide(1)"></span>
-			<span class = "dot" onclick="currentSlide(2)"></span>
-		</div>
-
 	<!------ Feature Products pt 1--------->
 	
 <br>
 			<div class="productContainer">
-						<h3> Featured Products </h3>
+						<h3> DISPLAY PRODUCTS </h3>
 				  <div class="row">
   					  <div class="col-1">
-  			    <img src="images/home1.png" alt="Image 1">
-					<h2> Seise Crew Neck </h2>
-						  <h2> $50.00 </h2>
-				
+  			    <img src="images/product1.png" alt="Image 1">
+					<h2> High Street Denim Jacket </h2>
+						  <h2> $190.00 </h2>
    		    </div>
  
 				  <div class="col-1">
-    		    <img src="images/home2.png" alt="Image 2">
-					<h2> Ackar Racing Jacket </h2>
-					    <h2> $70.00 </h2>
+    		    <img src="images/product2.png" alt="Image 2">
+					<h2> Red Sheep Wool Coat </h2>
+						  <h2> $140.00 </h2>
     		</div>
 					  
     		<div class="col-1">
-     		    <img src="images/home3.png" alt="Image 3">
-					<h2> Lower Hoodie </h2>
-				    <h2> $50.00 </h2>
+     		    <img src="images/product3.png" alt="Image 3">
+					<h2> Great Flames Hoodie </h2>
+						  <h2> $50.00 </h2>
   		   </div>
   		</div>
 	</div>
@@ -93,30 +62,33 @@
 				
   				<div class="row">
   				  <div class="col-1">
- 				     <img src="images/home4.png" alt="Image 1">
-						<h2> RPM NFG CO. JACKET </h2>
-					        <h2> $110.00 </h2>
+ 				     <img src="images/product4.png" alt="Image 1">
+						<h2> Quarter Zip Sweatshirt </h2>
+						  <h2> $90.00 </h2>					  
 				  </div>
 					
 				    <div class="col-1">
- 				     <img src="images/home5.png" alt="Image 2">
-						<h2> Deseyees Hoodie </h2>
-						  <h2> $90.00 </h2>
+ 				     <img src="images/product5.png" alt="Image 2">
+						<h2> Large Fleece Scarf </h2>
+						  <h2> $70.00 </h2>
  				   </div>
 					
  				   <div class="col-1">
-						    <img src="images/home6.png" alt="Image 3">
-						<h2> Cotton Fleece Zip Up Hoodie </h2>
-					               <h2> $90.00 </h2>
-					   		
+				     <img src="images/product6.png" alt="Image 3">
+						<h2> Partimento-Cut Off Hoodie </h2>
+						  <h2> $200.00 </h2>
 					   
  				  </div>
  				 </div>
+				
+				<h2>Images sourced from free use authors on Pinterest</h2>
+				
 				</div>
 
 			<div class = "footer">
-			<hr>
+				<hr>
 				<h2>Copyright Melvin Jessil 2023 ©</h2>
+	
 			</div>
 	
 	<a href = "#" class = "back-to-top">
--- form.php
+++ form.php
@@ -1,11 +1,11 @@
 <html>
 <head>
 	<title>PHP Form Design</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/style1.css">
+	<link rel = "shortcut icon" type = "image/png"  href = "images/favicon.png">
 </head>
 <body>
 		<div id = "main">
-			<h1>Contact</h1>
 			<hr>
 			<?php
 			//Pull links from the nav.php page and places them in the navigation bar div
@@ -14,13 +14,22 @@
 			<br><br><hr>
 			<?php
 			//Get the connection to the data base in phpMyAdmin
-				require_once("13dit_phpForm_mysqli.php");
+				require_once("13dit_iterationproject_MJessil_mysqli.php");
 			?>
 <br>
 	
 <!-- Add my Notepad++ form.html code here... -->		
 
-
+			
+		<div class = "logo">
+			
+			<!--<a href ="index.php">
+				<img src = "images/logo.png" width = "140px" height = "140px" >
+			</a>-->
+			<h3> Contact Us! </h3>
+		</div>
+			
+			
 	<!-- Form -->
 	<div class = "contact_container">
 		<form action ="connect.php" method="post">
@@ -28,43 +37,27 @@
 		<!-- Name Field -->
 
 		<div class ="row">
+	
 			<div class ="column">
-				<label for ="name=">Full Name</label>
-			</div>
-			<div class ="column2">
-				<input type ="text" id="name" name="name" placeholder ="Your Full name">
+				<input type ="text" id="name" name="name" placeholder ="Your First Name">
 			</div> 
 		</div>
-	
+			
 		<!-- Email Field -->
 	
 		<div class = "row">
-			<div class ="column">
-				<label for ="email">Email Address</label>
-			</div>
-			<div class = "column2">
+	
+			<div class = "column">
 				<input type = "text" id="email" name="email" placeholder = " Your Email Address">
 			</div>
 		</div>
 	
-		<!-- Contact Number Field -->
-	
-		<div class = "row">
-			<div class ="column">
-				<label for ="phone">Contact Number</label>
-			</div>
-			<div class = "column2">
-				<input type = "text" id="phone" name="phone" placeholder = "A Contact Number">
-			</div>
-		</div>
 		<!-- Subject Field -->
 		
 		<div class = "row">
-			<div class = "column">
-				<label for = "message">Subject</label>
-			</div>
-		<div class ="column2">
-		<textarea	 id ="message" name="message" placeholder="Write Something..." style="height:200px"></textarea>
+		
+		<div class ="column">
+		<textarea	 id ="message" name="message" placeholder="Enter Your Message" style="height:200px"></textarea>
 			</div>
 		</div>
 		
@@ -78,10 +71,13 @@
 	
 			</form>
 	
+	
+		
 	</div>
 		<!-- copyright statement -->
+		<hr>	
 		<div id ="footer">
-		&copy; Copyright Melvin Jessil, all rights reserved, Tawa College 2023
+			<h2>&copy; Copyright Melvin Jessil, all rights reserved, Tawa College 2023</h2>
 		</div>
 			
 	<body>
